a3ac109068b04ac94676ba1859263c883ad7773f
Kristijan Jakimov <kiko.jakimov@gmail.com>
2024-09-03 13:47:05 +0200

    rest of functions and tests added

--- passwordValidator/src/main/java/org/example/Main.java
+++ passwordValidator/src/main/java/org/example/Main.java
@@ -1,17 +1,19 @@
 package org.example;
-
-import javax.lang.model.type.TypeKind;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-
-
+    System.out.println(isCommonlyUsedPasswords("password"));
+    System.out.println(isCommonlyUsedPasswords("asasfasfasdf"));
     }
 
+    //Minimum Length
     public static boolean isItEight (String pass) {
         return pass.length() >= 8 ? true : false;
     }
 
+    //Digits
     public static boolean isDigitIncluded(String pass) {
         for (char c : pass.toCharArray()) {
             if (Character.isDigit(c)) {
@@ -21,10 +23,42 @@ public class Main {
         return false;
     }
 
+    //Upper or Lower
+        public static boolean isLowerAndUpper (String pass) {
+        boolean isLower = false;
+        boolean isUpper = false;
+            for (char c : pass.toCharArray()) {
+                if (Character.isUpperCase(c)) {
+                    isUpper = true;
+                }
+                if (Character.isLowerCase(c)) {
+                    isLower = true;
+                }
+        }
+            if (isLower && isUpper){
+                return true;
+            }
+            else return false;
+    }
+    //Common passwords
 
-//        public static boolean isLowerAndUpper (String pass) {}
-//        public static void commonlyUsedPasswords (String pass) {}
+    private static final Set<String> COMMONLY_USED_PASSWORDS = new HashSet<>();
 
+    static {
+        // Add commonly used weak passwords to the set
+        COMMONLY_USED_PASSWORDS.add("password");
+        COMMONLY_USED_PASSWORDS.add("123456");
+        COMMONLY_USED_PASSWORDS.add("password1");
+        COMMONLY_USED_PASSWORDS.add("123456789");
+        COMMONLY_USED_PASSWORDS.add("12345678");
+        COMMONLY_USED_PASSWORDS.add("qwerty");
+        COMMONLY_USED_PASSWORDS.add("abc123");
+        COMMONLY_USED_PASSWORDS.add("Password1");
+        COMMONLY_USED_PASSWORDS.add("Aa345678");
 
+}
+    public static boolean isCommonlyUsedPasswords(String pass) {
+        return COMMONLY_USED_PASSWORDS.contains(pass);
+    }
 }
 
--- passwordValidator/src/test/java/org/example/MainTest.java
+++ passwordValidator/src/test/java/org/example/MainTest.java
@@ -24,13 +24,40 @@ class MainTest {
     @Test
     void isDigitExpectTrue() {
         boolean given = true;
-        boolean expected = Main.isItEight("myNewPassword8");
+        boolean expected = Main.isDigitIncluded("myNewPassword8");
         assertEquals(given, expected);
     }
     @Test
     void isDigitExpectFalse() {
         boolean given = false;
-        boolean expected = Main.isItEight("passw");
+        boolean expected = Main.isDigitIncluded("passw");
+        assertEquals(given, expected);
+    }
+    //Is Upper and Lower case Included?
+    @Test
+    void isUpperLowerExpectTrue() {
+        boolean given = true;
+        boolean expected = Main.isLowerAndUpper("myNewPassword8");
+        assertEquals(given, expected);
+    }
+    @Test
+    void isUpperLowerExpectFalse() {
+        boolean given = false;
+        boolean expected = Main.isLowerAndUpper("passw");
+        assertEquals(given, expected);
+    }
+
+    //Is it commonly used one?
+    @Test
+    void isCommonlyUsedExpectTrue() {
+        boolean given = true;
+        boolean expected = Main.isCommonlyUsedPasswords("password");
+        assertEquals(given, expected);
+    }
+    @Test
+    void isCommonlyUsedExpectFalse() {
+        boolean given = false;
+        boolean expected = Main.isCommonlyUsedPasswords("pasasfsafsw");
         assertEquals(given, expected);
     }
 }
