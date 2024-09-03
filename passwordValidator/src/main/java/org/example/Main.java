3e6ff51a649c6e134d9c969f262cecc2848c2941
Kristijan Jakimov <kiko.jakimov@gmail.com>
2024-09-03 13:12:20 +0200

    Tests for isItEight and isDigitIncluded added

--- passwordValidator/src/main/java/org/example/Main.java
+++ passwordValidator/src/main/java/org/example/Main.java
@@ -1,7 +1,30 @@
 package org.example;
 
+import javax.lang.model.type.TypeKind;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!!");
+
+
     }
+
+    public static boolean isItEight (String pass) {
+        return pass.length() >= 8 ? true : false;
+    }
+
+    public static boolean isDigitIncluded(String pass) {
+        for (char c : pass.toCharArray()) {
+            if (Character.isDigit(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+//        public static boolean isLowerAndUpper (String pass) {}
+//        public static void commonlyUsedPasswords (String pass) {}
+
+
 }
+
--- passwordValidator/src/test/java/org/example/MainTest.java
+++ passwordValidator/src/test/java/org/example/MainTest.java
@@ -1,13 +1,36 @@
 package org.example;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
+
+    //Is it eight characters?
     @Test
-    void main_expectHelloWorld() {
-        Assertions.assertTrue(true);
+    void isItEightExpectTrue() {
+        boolean given = true;
+        boolean expected = Main.isItEight("myNewPassword");
+        assertEquals(given, expected);
+    }
+    @Test
+    void isItEightExpectFalse() {
+        boolean given = false;
+        boolean expected = Main.isItEight("passw");
+        assertEquals(given, expected);
+    }
+
+    //Is digit included?
+    @Test
+    void isDigitExpectTrue() {
+        boolean given = true;
+        boolean expected = Main.isItEight("myNewPassword8");
+        assertEquals(given, expected);
+    }
+    @Test
+    void isDigitExpectFalse() {
+        boolean given = false;
+        boolean expected = Main.isItEight("passw");
+        assertEquals(given, expected);
     }
 }
